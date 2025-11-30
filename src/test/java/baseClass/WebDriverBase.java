a8bb454146aabc2b6183b42aa63e84178305bf53
DivyaNamoju <86409027+DivyaNamoju@users.noreply.github.com>
2025-11-30 17:48:03 +0000

    Run tests on Selenium Grid

--- src/test/java/baseClass/WebDriverBase.java
+++ src/test/java/baseClass/WebDriverBase.java
@@ -2,15 +2,20 @@ package baseClass;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import utilities.ReadConfigProperties;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 public class WebDriverBase
@@ -22,18 +27,33 @@ public class WebDriverBase
     {
         log = LogManager.getLogger(this.getClass());
         final String BROWSER_NAME = ReadConfigProperties.getProperty("BROWSER");
-        System.out.println("Setting up things to run on =====> " + BROWSER_NAME);
-        driver = switch(BROWSER_NAME.toLowerCase()){
-            case "chrome" -> new ChromeDriver();
-            case "edge" -> new EdgeDriver();
-            case "firefox" -> new FirefoxDriver();
-            case "headless" -> {
-                ChromeOptions cp = new ChromeOptions();
-                cp.addArguments("--headless=new");
-                yield new ChromeDriver(cp);
+        final String EXECUTION_ENV = ReadConfigProperties.getProperty("EXECUTION_ENV");
+        if(EXECUTION_ENV.equalsIgnoreCase("remote"))
+        {
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+            desiredCapabilities.setPlatform(Platform.WIN11);
+            desiredCapabilities.setBrowserName(BROWSER_NAME);
+
+            try {
+                driver = new RemoteWebDriver(new URL("http://192.168.1.39:4444/wd/hub"), desiredCapabilities);
+            } catch (MalformedURLException e) {
+                System.out.println(e.getLocalizedMessage());
             }
-            default -> new ChromeDriver();
-        };
+        }
+        else {
+            System.out.println("Setting up things to run on =====> " + BROWSER_NAME);
+            driver = switch (BROWSER_NAME.toLowerCase()) {
+                case "chrome" -> new ChromeDriver();
+                case "edge" -> new EdgeDriver();
+                case "firefox" -> new FirefoxDriver();
+                case "headless" -> {
+                    ChromeOptions cp = new ChromeOptions();
+                    cp.addArguments("--headless=new");
+                    yield new ChromeDriver(cp);
+                }
+                default -> new ChromeDriver();
+            };
+        }
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.manage().window().maximize();
